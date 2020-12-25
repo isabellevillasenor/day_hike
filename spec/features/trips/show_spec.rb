c64b0a885b79a0687983f916678d9e7a017de4ed
Isabelle Villasenor <villasenori@outlook.com>
2020-12-25 13:57:48 -0700

    Add Trip Show Spec

---
+++ spec/features/trips/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Trips Show Page' do
+  before :each do
+    @t1 = Trip.create!(name: 'Trip 1', start_date: '2020-02-02 08:00:00', end_date: '2020-02-02 17:00:00')
+    @t2 = Trip.create!(name: 'Trip 2', start_date: '2020-02-02 08:00:00', end_date: '2020-02-02 17:00:00')
+  end
+
+  describe 'As A Visitor' do
+
+    it 'should show the name, start_date and end_date' do
+      visit "/trips/#{@t1.id}"
+
+      expect(page).to have_content(@t1.name)
+      expect(page).to have_content(@t1.start_date)
+      expect(page).to have_content(@t1.end_date)
+
+      expect(page).to have_no_content(@t2.name)
+      expect(page).to have_no_content(@t2.start_date)
+      expect(page).to have_no_content(@t2.end_date)
+    end
+    
+  end
+end
