8463ce7f1645de68de983e5ab432f674a30d0f65
Isabelle Villasenor <villasenori@outlook.com>
2020-12-26 07:58:58 -0700

    Add Dependent Destory to Trip and Trail Models

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,5 +1,5 @@
 class Trail < ApplicationRecord
-  has_many :trip_trails
+  has_many :trip_trails, dependent: :destroy
   has_many :trips, through: :trip_trails
   validates_presence_of :name, :address, :length
   validates_numericality_of :length, only_integer: true, greater_than: 0
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,9 @@
 class Trip < ApplicationRecord
-  has_many :trip_trails
+  has_many :trip_trails, dependent: :destroy
   has_many :trails, through: :trip_trails
   validates_presence_of :name, :start_date, :end_date
 
-
+  def all_trails
+    trails.all
+  end
 end
